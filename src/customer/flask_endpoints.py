a762500187558daa0d37acf92b8d301efd366681
Cesar Smaniotto Júnior <cesarsjb@gmail.com>
2020-09-06 20:10:24 -0300

    refactor mapping between url and flask view class

--- src/customer/flask_endpoints.py
+++ src/customer/flask_endpoints.py
@@ -44,6 +44,6 @@ class CustomerView(MethodView):
         return jsonify({}), 204
 
 
-class CustomerWithList(MethodView):
+class CustomerWishListView(MethodView):
     def post(self, customer_id, product_id):
         return jsonify({'wish_list': [product_id]}), 201
--- src/flask_app.py
+++ src/flask_app.py
@@ -2,21 +2,40 @@ import logging
 
 from flask import Flask
 
-from src.customer.flask_endpoints import CustomersView, CustomerView, CustomerWithList
+from src.customer.flask_endpoints import (
+    CustomersView,
+    CustomerView,
+    CustomerWishListView,
+)
 from src.orm import metadata
 
 
 def create_app():
     app = Flask(__name__)
     app.config['DEBUG'] = True
-    app.add_url_rule('/customers', view_func=CustomersView.as_view('customers'))
-    app.add_url_rule('/customers/<int:id>', view_func=CustomerView.as_view('customer'))
-    app.add_url_rule(
-        '/customers/<int:customer_id>/wish_list/<string:product_id>',
-        view_func=CustomerWithList.as_view('customer_wish_list'),
-    )
+
+    register_urls(app)
+
     app.register_error_handler(Exception, handle_exception)
+
     return app
+
+
+def register_urls(app):
+    url_view_cls = (
+        ('/customers', CustomersView),
+        ('/customers/<int:id>', CustomerView),
+        (
+            '/customers/<int:customer_id>/wish_list/<string:product_id>',
+            CustomerWishListView,
+        ),
+    )
+    for url, view_cls in url_view_cls:
+        register_url(app, url, view_cls)
+
+
+def register_url(app, url, view_cls):
+    app.add_url_rule(url, view_func=view_cls.as_view(view_cls.__name__))
 
 
 def handle_exception(exception):
