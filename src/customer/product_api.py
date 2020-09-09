d9ce485ead5b83d10e2dc0c9af94815f561b02a1
Cesar Smaniotto Júnior <cesarsjb@gmail.com>
2020-09-08 22:45:54 -0300

    remove magic number of product api

--- src/customer/product_api.py
+++ src/customer/product_api.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 import requests
 
 from src.config import PRODUCT_API_BASE_URL
@@ -6,4 +8,4 @@ from src.config import PRODUCT_API_BASE_URL
 def exists_product_with_id(product_id):
     url = f'{PRODUCT_API_BASE_URL}/api/product/{product_id}/'
     response = requests.get(url)
-    return response.status_code == 200
+    return response.status_code == HTTPStatus.OK
